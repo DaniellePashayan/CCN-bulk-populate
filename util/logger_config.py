f41cafd659d16fb9bed5d2b9b84fa7efdf27f69c
Danielle Pashayan <daniellepashayan@gmail.com>
2023-10-12 08:31:25 -0400

    updated log path to mdrive

--- util/logger_config.py
+++ util/logger_config.py
@@ -1,5 +1,7 @@
 from loguru import logger
 
-logger.add("./logs/{time:YYYY-MM-DD}.log",
+mdrive = r'M:\CPP-Data\Sutherland RPA\Northwell Process Automation ETM Files\Monthly Reports\Charge Correction\New vs Established\Logs'
+
+logger.add(f"{mdrive}/{time:YYYY-MM-DD}.log",
            format="{time:YYYY-MM-DD at HH:mm:ss} | {level} - {message}",
            colorize=True, backtrace=True, diagnose=True, level='INFO', retention='90 days')
