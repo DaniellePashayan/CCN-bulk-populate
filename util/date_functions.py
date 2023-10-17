a7d16f014d8351b6970f0cdecd7ad7e110afe9de
Danielle Pashayan <daniellepashayan@gmail.com>
2023-10-17 14:47:10 -0400

    updated date_functions to account for last bus day

--- util/date_functions.py
+++ util/date_functions.py
@@ -6,51 +6,62 @@ from loguru import logger
 from pandas.tseries.holiday import USFederalHolidayCalendar
 
 
-def check_weekend(date):
-    # 5 = Saturday, 6 = Sunday
-    return date.weekday() > 4
+def check_if_weekend(date):
+    """
+    Checks if date is a weekend
+    """
+    # check if date is a datetime object
+    if isinstance(date, datetime.datetime):
+        return date.weekday() >= 5
+    elif isinstance(date, str):
+        return pd.to_datetime(date).weekday() >= 5
 
 def check_if_holiday(date):
-    cal = USFederalHolidayCalendar()
-    holidays = cal.holidays(start='2023-01-01', end='2024-12-31').to_pydatetime()
-    holidays = [(pd.to_datetime(x) + datetime.timedelta(days=1)) for x in holidays]
-    # need to remove juneteenth
-    excluded_holidays = [datetime.date(x.year, 6, 19) for x in holidays if x.year == 2023]
-    
-    holidays = [x for x in holidays if x.date() not in excluded_holidays]
+    if isinstance(date, str):
+        date = pd.to_datetime(date)
+        
+    custom_holidays = {
+    '2023-01-02': 'New Years Day',
+    '2023-01-16': 'Martin Luther King Jr. Day',
+    '2023-02-20': 'Presidents Day',
+    '2023-05-29': 'Memorial Day',
+    '2023-07-04': 'Independence Day',
+    '2023-09-04': 'Labor Day',
+    '2023-11-23': 'Thanksgiving Day',
+    '2023-12-25': 'Christmas Day',
+    '2024-01-01': 'New Years Day',
+    }
+    holidays_df = pd.DataFrame(list(custom_holidays.items()), columns=['Date', 'Holiday Name'])
+    holidays_df['Date'] = pd.to_datetime(holidays_df['Date'])
+    holidays = holidays_df['Date'].to_list()    
+
     return date in holidays
 
 def check_if_EOM(date):
-    # check if the date is the last day of the month
-    last_day_of_month = (date.replace(day=28) + datetime.timedelta(days=4)).replace(day=1) - datetime.timedelta(days=1)
-    return last_day_of_month == date
+    if isinstance(date, str):
+        date = pd.to_datetime(date)
+        
+    return date.day >= calendar.monthrange(date.year, date.month)[1] - 1
 
-def check_if_within_3_days_of_EOM(date):
-    # check if the date is the last day 3 days of the month
-    last_day_of_month = (date.replace(day=28) + datetime.timedelta(days=4)).replace(day=1) - datetime.timedelta(days=1)
+def check_last_business_day_of_month(date):
+    if isinstance(date, str):
+        date = pd.to_datetime(date)
+    # get the first day of the month
+    FOM = date.replace(day=1)
+    EOM = FOM + pd.offsets.MonthEnd(0)
+    # return a list of all the weekdays in the month
+    last_weekday = [d for d in pd.date_range(FOM, EOM) if d.weekday() < 5][-1]
+    return last_weekday.weekday() == 4 and date == last_weekday
     
-    tomorrow = date + datetime.timedelta(days=1)
-    
-    is_friday = tomorrow.weekday() == 4
-
-    if is_friday:
-        within_3_days = (last_day_of_month - date).days <= 2
-        return within_3_days
-    else:
-        return False
-    # print(within_3_days)
-    # print(is_thursday)
-    return 
+def check_if_EOM(date):
+    return date.day == calendar.monthrange(date.year, date.month)[1]
 
 def get_next_business_day(today):
-    
     if isinstance(today, str):
         today = pd.to_datetime(today)
     
-    tomorrow = today + datetime.timedelta(days=1) 
+    tomorrow = today + datetime.timedelta(days=1)
 
-    # if tomorrow is a friday and day + 2 or day + 3 is EOM   
-    while check_weekend(today) or check_if_holiday(today) or check_if_EOM(today) or check_if_within_3_days_of_EOM(tomorrow):
+    while check_if_weekend(tomorrow) or check_if_holiday(tomorrow) or check_if_EOM(tomorrow) or check_last_business_day_of_month(tomorrow):
         tomorrow += datetime.timedelta(days=1)
-        tomorrow = pd.to_datetime(tomorrow)
     return tomorrow
